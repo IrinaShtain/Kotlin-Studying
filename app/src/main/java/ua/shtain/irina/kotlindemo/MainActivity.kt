275a6f806b55dea82fb2fb2d8c0fa5367d206148
IrinaShtain <shtainirina@gmail.com>
2018-01-15 14:37:08 +0200

    first commit

---
+++ app/src/main/java/ua/shtain/irina/kotlindemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package ua.shtain.irina.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tvMsg = findViewById<TextView>(R.id.tvKotlinHello)
+        tvMsg.text = "Hello Kotlin"
+       
+    }
+}
