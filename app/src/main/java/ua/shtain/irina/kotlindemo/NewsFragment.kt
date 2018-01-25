22b9e588a925d5db8a906cabd2fd071eb8605d17
IrinaShtain <shtainirina@gmail.com>
2018-01-25 15:38:00 +0200

    create Infinite Scroll behaviour

--- app/src/main/java/ua/shtain/irina/kotlindemo/NewsFragment.kt
+++ app/src/main/java/ua/shtain/irina/kotlindemo/NewsFragment.kt
@@ -12,6 +12,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.news_fragment.*
+import ua.shtain.irina.kotlindemo.commons.InfiniteScrollListener
+import ua.shtain.irina.kotlindemo.commons.RedditNews
 import ua.shtain.irina.kotlindemo.commons.adapters.NewsAdapter
 import ua.shtain.irina.kotlindemo.commons.inflate
 
@@ -21,16 +23,15 @@ import ua.shtain.irina.kotlindemo.commons.inflate
 class NewsFragment : Fragment() {
     private val newsManager by lazy { NewsManager() }
     var subscriptions = CompositeDisposable()
+    private var redditNews: RedditNews? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             container?.inflate(R.layout.news_fragment)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        rvNewsList.setHasFixedSize(true)
-        rvNewsList.layoutManager = LinearLayoutManager(context)
+        setupRecyclerView()
         initAdapter()
-
         if (savedInstanceState == null) {
             requestNews()
         }
@@ -46,12 +47,23 @@ class NewsFragment : Fragment() {
         subscriptions.clear()
     }
 
+    private fun setupRecyclerView() {
+        rvNewsList.setHasFixedSize(true)
+        val linearLayout = LinearLayoutManager(context)
+        rvNewsList.layoutManager = linearLayout
+        rvNewsList.clearOnScrollListeners()
+        rvNewsList.addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
+    }
+
     private fun requestNews() {
-        subscriptions.add(newsManager.getNews()
+        subscriptions.add(newsManager.getNews(redditNews?.after ?: "")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        (rvNewsList.adapter as NewsAdapter)::addNews,
+                        { retrievedNews ->
+                            redditNews = retrievedNews
+                            (rvNewsList.adapter as NewsAdapter).addNews(retrievedNews.news)
+                        },
                         { e: Throwable ->
                             Toast.makeText(this.context, "error " + e.message, Toast.LENGTH_LONG).show()
                         }
--- app/src/main/java/ua/shtain/irina/kotlindemo/NewsManager.kt
+++ app/src/main/java/ua/shtain/irina/kotlindemo/NewsManager.kt
@@ -3,24 +3,31 @@ package ua.shtain.irina.kotlindemo
 import io.reactivex.Observable
 import ua.shtain.irina.kotlindemo.commons.RedditNewsItem
 import ua.shtain.irina.kotlindemo.api.RestAPI
+import ua.shtain.irina.kotlindemo.commons.RedditNews
 
 /**
  * Created by Irina Shtain on 24.01.2018.
  */
 class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
+    fun getNews(after: String, limit: String = "10"): Observable<RedditNews> {
         return Observable.create { subscriber ->
-            val callResponse = api.getNews("", limit)
+            val callResponse = api.getNews(after, limit)
             val response = callResponse.execute()
 
             if (response.isSuccessful) {
-                val news = response.body()?.data?.children?.map {
+                val dataResponse = response.body()?.data
+                val news = dataResponse?.children?.map {
                     val item = it.data
                     RedditNewsItem(item.author, item.title, item.num_comments,
                             item.created, item.thumbnail, item.url)
                 }
-                news?.let { subscriber.onNext(it) }
+                val redditNews = RedditNews(
+                        dataResponse?.after ?: "",
+                        dataResponse?.before ?: "",
+                        news!!)
+
+                subscriber.onNext(redditNews)
                 subscriber.onComplete()
             } else {
                 subscriber.onError(Throwable(response.message()))
